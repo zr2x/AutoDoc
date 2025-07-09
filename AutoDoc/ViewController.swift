cc7ae7c34522866ecabafb34f9483128c658f55a
Iskander Sitdikov <master5212345@gmail.com>
2025-07-09 14:26:59 +0500

    Initial Commit

---
+++ AutoDoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoDoc
+//
+//  Created by Искандер Ситдиков on 09.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
